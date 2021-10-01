8de858b06253698b2bb78594cf905f6e639c84c4
mamta-y <mamtayadav987gmail.com>
2021-10-01 17:11:50 +0530

    done

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -3,7 +3,19 @@ const jwt = require('jsonwebtoken');
 const app = express();
 const gateway= require('fast-gateway');
 const port = 9090;
-
+// function auth(req,res,next){
+//     const token= req.header('auth-token');//
+//      if(!token) return res.status(401).send('access deneid......no token provided');
+//      try{
+//     const decodedtoken=jwt.verify(token,'Secretkey')
+//     console.log(decodedtoken);
+//     req.user=decodedtoken
+//     next();
+//     }
+//     catch(err){
+//         res.status(400).send('invalid token.....')
+//     }}
+    
         
 
     
